6840ff9aefdc6f82f570bf03705cb1e3c3b3af60
Gordon <JGordon@corp.group.local>
2017-01-17 10:56:09 +0000

    Change for lesson - removed security

--- svc/public/webapp/app.js
+++ svc/public/webapp/app.js
@@ -1,28 +1,8 @@
 "use strict";
 (function () {
 
-    var app = angular.module("app", ["ui.router", "securityManager"]).run(
+    var app = angular.module("app", ["ui.router"]).run(
         function ($window, $rootScope, $log, $location) {
-            $rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams) {
-                if(fromState.url.indexOf("/") > -1)
-                {
-                    if(fromState.url !== $location.url())
-                    {
-                        event.preventDefault();
-                    }
-                }
-            });
-
-
-            $rootScope.$$errorText = $$errorText;
-            $rootScope.$$dataType = $$dataType;
-
             $log.debug("App Instantiated");
         });
-
-    //pushes token onto each http request header
-    app.config(function ($httpProvider) {
-        $httpProvider.interceptors.push('authInterceptor');
-    });
-
 }());
--- svc/public/webapp/feature/login/login-controller.js
+++ svc/public/webapp/feature/login/login-controller.js
@@ -6,9 +6,9 @@
      */
     angular.module("app")
         .controller("loginController",
-        ["$state", "$window", "securityManager",   LoginCtrl]);
+        ["$state", "$window", LoginCtrl]);
 
-    function LoginCtrl($state, $window, securityManager) {
+    function LoginCtrl($state, $window) {
         var vm = this;
 
         vm.hasValidationError = false;
@@ -23,16 +23,7 @@
             vm.hasValidationError = false;
             vm.hasAuthenticationError = false;
             vm.credentials = {username: vm.username, password: vm.password};
-            securityManager.login(vm.credentials).then(function (result) {
-                    $window.sessionStorage.token = result.token;
-                    $state.go("home.dashboard");
-                },
-                function (e) {
-                    delete $window.sessionStorage.token;
-                    vm.hasValidationError = true;
-                    vm.hasAuthenticationError = true;
-                    $state.go("login");
-                });
+            $state.go("home.dashboard");
         };
     }
 }());
